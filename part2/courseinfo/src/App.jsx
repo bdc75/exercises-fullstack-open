4f5f9f67f0e374052deec9b18779248432137e26
Benjamin Cardwell <bencardwell1999@gmail.com>
2024-08-06 16:44:02 -0400

    Exercise 2.4 complete

--- part2/courseinfo/src/App.jsx
+++ part2/courseinfo/src/App.jsx
@@ -1,28 +1,78 @@
 const App = () => {
-  const course = {
-    id: 1,
-    name: 'Half Stack application development',
-    parts: [
-      {
-        name: 'Fundamentals of React',
-        exercises: 10,
-        id: 1
-      },
-      {
-        name: 'Using props to pass data',
-        exercises: 7,
-        id: 2
-      },
-      {
-        name: 'State of a component',
-        exercises: 14,
-        id: 3
-      }
-    ]
-  }
+  // const course = {
+  //   id: 1,
+  //   name: 'Half Stack application development',
+  //   parts: [
+  //     {
+  //       name: 'Fundamentals of React',
+  //       exercises: 10,
+  //       id: 1
+  //     },
+  //     {
+  //       name: 'Using props to pass data',
+  //       exercises: 7,
+  //       id: 2
+  //     },
+  //     {
+  //       name: 'State of a component',
+  //       exercises: 14,
+  //       id: 3
+  //     }
+  //   ]
+  // }
+  const courses = [
+    {
+      name: 'Half Stack application development',
+      id: 1,
+      parts: [
+        {
+          name: 'Fundamentals of React',
+          exercises: 10,
+          id: 1
+        },
+        {
+          name: 'Using props to pass data',
+          exercises: 7,
+          id: 2
+        },
+        {
+          name: 'State of a component',
+          exercises: 14,
+          id: 3
+        },
+        {
+          name: 'Redux',
+          exercises: 11,
+          id: 4
+        }
+      ]
+    }, 
+    {
+      name: 'Node.js',
+      id: 2,
+      parts: [
+        {
+          name: 'Routing',
+          exercises: 3,
+          id: 1
+        },
+        {
+          name: 'Middlewares',
+          exercises: 7,
+          id: 2
+        }
+      ]
+    }
+  ]
   console.log(new Date())
   
-  return <Course course={course} />
+  return (
+    <div>
+      {courses.map((course) => (
+        <Course course={course}/>
+      ))}
+    </div>
+  )
 }
 
 const Course = ({ course }) => (
@@ -52,7 +102,6 @@ const Content = (props) => {
 
 const Total = (props) => {
   const parts = props.course.parts
-  // I used reduce in 2.2
   const sumExerc = parts.reduce(
     (sum, part) => sum + part.exercises, 0
   )
