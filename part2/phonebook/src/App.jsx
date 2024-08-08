00e368276d0b869b3d835af52da4933bcada37b4
Benjamin Cardwell <bencardwell1999@gmail.com>
2024-08-08 11:35:56 -0400

    Exercise 2.11 complete

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,5 +1,6 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import Person from './components/Person'
+import axios from 'axios'
 
 const Filter = ({ onChange }) => {
   return <div>filter shown with: <input onChange={onChange}/></div>
@@ -25,14 +26,19 @@ const PersonForm = (props) => (
 )
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456', id: 1 },
-    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
-  ])
+  const [persons, setPersons] = useState([])
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
+
+  useEffect(() => {
+    console.log('effect');
+    axios
+      .get('http://localhost:3001/persons')
+      .then(response => {
+        console.log('promise fulfilled')
+        setPersons(response.data)
+      })
+  }, [])
 
   const handleNameChange = (event) => setNewName(event.target.value)
   const handleNumberChange = (event) => setNewNumber(event.target.value)
@@ -53,7 +59,7 @@ const App = () => {
       alert(`${newName} is already in the phonebook`)
   }
 
-  
+  console.log('render persons as: ', persons)
   return (
     <div>
       <h2>Phonebook</h2>
