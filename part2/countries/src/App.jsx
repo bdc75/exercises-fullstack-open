e171671cc72bd1191f5d606f5414f0b9b692e534
Benjamin Cardwell <bencardwell1999@gmail.com>
2024-08-09 17:06:03 -0400

    Exercise 2.18 completed

--- part2/countries/src/App.jsx
+++ part2/countries/src/App.jsx
@@ -3,26 +3,46 @@ import axios from 'axios'
 
 const countriesURL = "https://studies.cs.helsinki.fi/restcountries/api/all"
 
+const Languages = ({ langs }) => (
+  <div>
+    <strong>languages:</strong>
+    <ul>
+      {Object.entries(langs).map(
+        entry => <li key={entry[0]}>{entry[1]}</li>
+      )}
+    </ul>
+  </div>
+)
+
 const CountryProfile = ({ country }) => {
-  const cpStyle = {fontSize : 200}
-  return (
-    <div style={cpStyle}>
-      {country.flag}
+  console.log('country', country)
+  if (!country)
+    return null
+  else return (
+    <div style={{padding: 0}}>
+      <h2>{country.name.common}</h2>
+      <p>capital {country.capital}</p>
+      <p>area {country.area}</p>
+      <Languages langs={country.languages} />
+      <img src={country.flags.png} alt={country.flags.alt}/>
     </div>
   )
 }
 
 const CountryForm = ({ search, onChange }) => (
   <div>
-    find countries <input value={search} onChange={onChange}/>
+    find countries
+    <input value={search} onChange={onChange}/>
+    <button value={search} onClick={onChange}>refresh search</button>
   </div>
 )
 
-const Results = ({ filteredResults }) => {
-  const resStyle = {listStyleType: "none"}
+const Results = ({ filteredResults, setResults }) => {
+  const resStyle = {listStyleType: "none", padding: 0}
 
   const len = filteredResults.length
   console.log('length of results:', len)
+  
   if (len === 0) {
     return null
   } else if (len > 10) {
@@ -30,47 +50,54 @@ const Results = ({ filteredResults }) => {
   } else if (len > 1) {    
     return (
       <ul style={resStyle}>
-        {filteredResults.map(country => <li key={country.name.official}>{country.name.common}</li>)}
+        {filteredResults.map(country => 
+          <li key={country.name.official}>
+            {country.name.common}
+            <button onClick={() => setResults([country])}>show</button>
+          </li>
+        )}
       </ul>
     )
   } 
   else {
     return <CountryProfile country={filteredResults[0]}/>
+    // onShow(filteredResults[0])
   }
 }
 
 const App = () => {
   const [search, setSearch] = useState('')
   // const [numResults, setNumResults] = useState(0)
-  const [initialResults, setInitialResults] = useState({})
-  const [filteredResults, setFilteredResults] = useState({})
+  const [initialResults, setInitialResults] = useState([])
+  const [filteredResults, setFilteredResults] = useState([])
+  // const [shownCountry, setShownCountry] = useState(null)
 
+  // Set initial results once
   useEffect(() => {
     axios
     .get(countriesURL)
     .then(response => setInitialResults(response.data))
     }, []
   )
-  
-  // const countryNames = countries => countries.map(c => c.name)
 
   const handleSearch = (e) => {
-    console.log('etargetvalue', e.target.value)
     const search = e.target.value.toLowerCase()
     setSearch(search)
+    if (search.length === 0) {
+      setFilteredResults([])
+      return
+    }
     setFilteredResults(initialResults
       .filter(
         country => country.name.common.toLowerCase().includes(search)
     ))
   }
 
-
-  console.log(initialResults)
   return (
-    <div>
+    <div style={{backgroundColor: 'powderblue'}}>
       <CountryForm search={search} onChange={handleSearch}/>
-      <Results filteredResults={filteredResults}></Results>
-      {/* <CountryProfile></CountryProfile> */}
+      <Results filteredResults={filteredResults} setResults={setFilteredResults}/>
+      {/* <CountryProfile country={shownCountry}/> */}
     </div>
   )
 }
